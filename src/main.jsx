b4e291863bc6b24e3912c0c43a9ee31a621dea20
Marjorie P. Matilos <marjorie.matilos20@gmail.com>
2026-04-16 02:09:52 +0800

    Adding analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'  // import '/theme/index.css'
-  import App from './App.jsx'
-  import { BrowserRouter } from 'react-router-dom'
-  import { HelmetProvider } from 'react-helmet-async';
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async';
 import './theme/index.css'
+import { Analytics } from '@vercel/analytics/react'
 
  ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
